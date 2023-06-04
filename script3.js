d1fb4ba34f6f4082e6e06e026f3a8bfec5d9c00a
Mohamadmahgoub910 <mohamadsaedali@hotmail.com>
2023-06-04 16:32:16 +0300

    handle buttons next,prev,slideShow,stop

--- script3.js
+++ script3.js
@@ -13,8 +13,59 @@ console.log("Welcome to Day4-1 Zo7lika");
 // change image on click
 var imgBtn = document.getElementById("chngImg");
 imgBtn.addEventListener("click", function () {
-  var img = document.querySelector("img");
+  var img = document.getElementById("chng");
   var oldSrc = img.getAttribute("src");
+  console.log(oldSrc);
   var c = oldSrc === "imgs/m.png" ? "imgs/img.jpg" : "imgs/m.png";
   img.setAttribute("src", c);
 });
+
+// Next Image and Previous Image
+var next = document.getElementById("next");
+var prev = document.getElementById("prev");
+var imgSlide = document.getElementById("imgSlide");
+var count = 0;
+var counted;
+// console.log(next);
+// console.log(prev);
+var ArrImgs = new Array();
+ArrImgs = [
+  "imgs/img.jpg",
+  "imgs/img1.jpg",
+  "imgs/img2.jpg",
+  "imgs/img3.jpg",
+  "imgs/img4.jpg",
+  "imgs/m.png",
+];
+next.addEventListener("click", function () {
+  var slid = document.getElementById("slide");
+  if (count <= 4) {
+    count++;
+    slid.setAttribute("src", ArrImgs[count]);
+  } else {
+    alert("Game Over");
+  }
+});
+prev.addEventListener("click", function () {
+  var slid = document.getElementById("slide");
+  if (count === 0) {
+    alert("Game Over");
+    count = 0;
+  } else {
+    count--;
+    slid.setAttribute("src", ArrImgs[count]);
+  }
+});
+function imgSlid() {
+  count++;
+  if (count == 5) {
+    count = 0;
+  }
+
+  var slid = document.getElementById("slide");
+  slid.setAttribute("src", ArrImgs[count]);
+  counted = setTimeout("imgSlid()", 500);
+}
+function stopSlide() {
+  clearTimeout(counted);
+}
