28b869ec4a61e9c0e1928c6286b540edf1a392bf
Mohamadmahgoub910 <mohamadsaedali@hotmail.com>
2023-06-04 15:19:52 +0300

    Change Img On Click

--- script3.js
+++ script3.js
@@ -1,11 +1,20 @@
 console.log("Welcome to Day4-1 Zo7lika");
 // get text
-var btn = document.querySelector("button");
-btn.addEventListener("click", function () {
-  var txt = document.querySelector("input").value;
-  var content = document.getElementsByClassName("content")[0];
-  if (txt !== "") {
-    content.innerText = txt;
-    content.style.backgroundColor = txt;
-  }
+// var btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//   var txt = document.querySelector("input").value;
+//   var content = document.getElementsByClassName("content")[0];
+//   if (txt !== "") {
+//     content.innerText = txt;
+//     content.style.backgroundColor = txt;
+//   }
+// });
+
+// change image on click
+var imgBtn = document.getElementById("chngImg");
+imgBtn.addEventListener("click", function () {
+  var img = document.querySelector("img");
+  var oldSrc = img.getAttribute("src");
+  var c = oldSrc === "imgs/m.png" ? "imgs/img.jpg" : "imgs/m.png";
+  img.setAttribute("src", c);
 });
